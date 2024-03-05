c7eb785ee3a0d03b18f0491eef63cc2c9ab83cc4
David Rhoades <david.isaac.rhoades@gmail.com>
2024-03-04 21:57:43 -0600

    created AACMappings.java and created all methods (stubs) with documentation

--- bin/README.md
+++ bin/README.md
@@ -2,3 +2,6 @@
 
 Method and class descriptions for AACCategory:
 https://accessibilityeducation.github.io/assignments/AAC/AACCategory.html
+
+Method and class descriptions for AACMappings:
+https://accessibilityeducation.github.io/assignments/AAC/AACMappings.html
---
+++ src/AACMappings.java
@@ -0,0 +1,80 @@
+
+
+
+/**
+ * An class to keep track of a coplete set of AAC mappings
+ * 
+ * @author David Rhoades
+ */
+public class AACMappings extends java.lang.Object{
+
+
+  /**
+   * Creates a new AACMappings in based on filename
+   * @param filename a formatted file
+   */
+  public AACMappings(String filename) {
+    //STUB
+  }
+
+
+  /**
+   * Adds the mapping to the current category (or the default category if that is the current category)
+   * @param imageLoc
+   * @param text
+   */
+  void add(String imageLoc, String text) {
+    //STUB
+  }
+
+  /**
+   * Gets the current category
+   * @return current category
+   */
+  String getCurrentCategory() {
+    return null;
+    //STUB
+  }
+
+  /**
+   * Provides an array of all the images in the current category
+   * @return an array of all the images in the current category
+   */
+  String[] getImageLocs() {
+    return null;
+  }
+
+  /**
+   * returns the text associated with the given image location
+   * @param imageLoc image of inquiry
+   * @return text associated with imageLoc
+   */
+  String getText(String imageLoc) {
+    return null;
+  }
+
+  /**
+   * Determines if the image represents a category or text to speak
+   * @param imageLoc image of inquiry
+   * @return true if the image represents a category or text, false otherwise
+   */
+  boolean isCategory(String imageLoc) {
+    return false;
+    //STUB
+  }
+
+  /**
+   * Resets the current category of the AAC back to the default category
+   */
+  void reset() {
+    //STUB
+  }
+
+  /**
+   * Writes the ACC mappings stored to a file.
+   * @param filename target file
+   */
+  void writeToFile(String filename) {
+    //STUB
+  }
+}
